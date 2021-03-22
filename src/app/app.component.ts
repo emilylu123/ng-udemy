d7b0c8577d723737cc68dc0259c03cdb062c5660
Yingyao Lu <emilylu123@gmail.com>
2021-03-22 22:01:20 +1030

    Passing Data around with custom event - eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   persons: string[] = ['Max', 'Emily', 'Ann'];
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
 }
